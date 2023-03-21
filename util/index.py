a009536dcd03f21818f782881d0c3c26f813bbaf
Qiming Hu <18002177060@163.com>
2023-03-21 10:02:06 +0800

    Update index.py
    handle version issues about skimage

--- util/index.py
+++ util/index.py
@@ -1,5 +1,10 @@
 # Metrics/Indexes
-from skimage.measure import compare_ssim, compare_psnr
+try:
+    from skimage.measure import compare_ssim, compare_psnr  
+except:
+    from skimage.metrics import peak_signal_noise_ratio as compare_psnr
+    from skimage.metrics import structural_similarity as compare_ssim
+    
 from functools import partial
 import numpy as np
 
